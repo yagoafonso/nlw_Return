3065d7ad32087355a8fd6013dd86924f1afd9541
Yago <yago.afonso@hotmail.com>
2023-01-21 18:48:53 -0400

    feat: criado componente header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
-import { Habit } from './components/Habit'
+import './styles/global.css';
 
-function App() {
+import { Header } from "./components/Header";
+
+
+export function App() {
 
   return (
-    <>
-      <Habit completed={10} />
-      <Habit completed={20} />
-      <Habit completed={30} />
-      <Habit completed={40} />  
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+       
+      <Header />
+
+      </div>
+    </div>
   )
 }
-
-export default App
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.tsx"],
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A'
+      },
+    },
   },
   plugins: [],
 }

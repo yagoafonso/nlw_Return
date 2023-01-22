e261a0f9a2c35da907211c7984c04d50a2dc4ba4
Yago <yago.afonso@hotmail.com>
2023-01-21 21:33:38 -0400

    feat: criado função para preencher quantidade minina de dias

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -13,6 +13,9 @@ const weekDays = [
 
 const summaryDates =generateDatesFromYearBeginning()
 
+const minimumSummaryDateSize = 18 * 7 //18 SEMANAS
+const amountOfDaysToFill = minimumSummaryDateSize -summaryDates.length
+
 export function Summarytable() {
   return (
     <div className="w-full flex">
@@ -31,6 +34,15 @@ export function Summarytable() {
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()} />
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, indice) =>{
+          return (
+            <div 
+              key={indice}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
+            />
+          )
         })}
       </div>      
     </div>

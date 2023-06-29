894bb24836e5be906584544b2779d9714427cf7d
Yago <yago.afonso@hotmail.com>
2023-06-29 18:46:49 -0300

    feat: configurado a rota de criação de hábito na versão mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,19 +1,59 @@
-import { View, Text, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, ScrollView, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import dayjs from "dayjs";
 
+import { api } from '../lib/axios';
 import { generateDatesFromYearBeginning } from "../utils/generate-range-from-year-beginning";
 
-import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { HabitDay, DAY_SIZE, DAY_MARGIN_BETWEEN } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateDatesFromYearBeginning();
 const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
+
+type SummaryProps = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>
+
 export function Home() {
 
+  const [loading, setLoading] = useState(true);
+  const [summary, setSummary] = useState<SummaryProps | null>(null); '  '
+
   const { navigate } = useNavigation();
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const response = await api.get('/summary');
+      console.log(response.data);
+      setSummary(response.data);
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos.');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -33,29 +73,40 @@ export function Home() {
       </View>
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: 100}}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
-        <View className="flex-row flex-wrap">
-          {
-            datesFromYearStart.map(date => (
-              <HabitDay
-                key={date.toISOString()}
-                onPress={() => navigate('habit', { date: date.toISOString() })}
-              />
-            ))
-          }
+        {
+          summary &&
+          <View className="flex-row flex-wrap">
+            {
+              datesFromYearStart.map(date => {
+                const dayWithHabits = summary.find(day => {
+                  return dayjs(date).isSame(day.date, 'day')
+                })
+                return (
+                  <HabitDay
+                    key={date.toISOString()}
+                    date={date}
+                    amountOfHabits={dayWithHabits?.amount}
+                    amountCompleted={dayWithHabits?.completed}
+                    onPress={() => navigate('habit', { date: date.toISOString() })}
+                  />
+                )
+              })
+            }
 
-          {
-            amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, indice) => (
-              <View
-                key={indice}
-                className="bg-zinc-400 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                style={{ width: DAY_SIZE, height: DAY_SIZE }}
-              />
-            ))
-          }
-        </View>
-      </ScrollView>
+            {
+              amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, indice) => (
+                <View
+                  key={indice}
+                  className="bg-zinc-400 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                />
+              ))
+            }
+          </View>
+        }
+        </ScrollView>
     </View>
   );
 }
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domigo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 export function New() {
+  const [title, setTitle] = useState('')
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function handleToggleWeekDay(weekDayIndex: number){
@@ -18,7 +20,22 @@ export function New() {
       setWeekDays(prevState =>[...prevState, weekDayIndex]);
     }
   }
+  async function handleCreateNewHabit() {
+    try {
+      if(!title.trim() || weekDays.length === 0){
+        Alert.alert('Novo Hábito', 'Informe o nome do hábito e selecione os dias da semana');
+      }
+      await api.post('/habits', { title, weekDays });
 
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso')
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito!');
+    }
+  }
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView 
@@ -40,6 +57,8 @@ export function New() {
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base" >
@@ -59,6 +78,7 @@ export function New() {
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather
             name="check"
